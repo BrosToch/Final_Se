6481616df2cc366dd63161dfa28a7c8d8475dcde
Bros Toch <brostoch0031@gmail.comm>
2022-06-13 12:09:02 +0700

    just push image as the answer in folder image

--- src/main/java/gic/i4gic/tp/app/AllTasksController.java
+++ src/main/java/gic/i4gic/tp/app/AllTasksController.java
@@ -16,11 +16,9 @@ public class AllTasksController {
 
 		return """
 				<center>
-				<a href="http://localhost:8080/tp14/task1"><h1> Task1 </h1><a>
-				<a href="http://localhost:8080/tp14/task2"><h1> Task2 </h1><a>
-				<a href="http://localhost:8080/tp14/task3"><h1> Task3 </h1><a>
-				<a href="http://localhost:8080/tp14/task4"><h1> Task4 </h1><a>
-				<a href="http://localhost:8080/tp14/task5?username=Gest"><h1> Task5 </h1><a>
+		
+				<a href="http://localhost:8080/tp14/task2"><h1> Go to login</h1><a>
+			
 				<br>
 				<h2 style="color:Red"> Noted: Before go to Task5, Please login in Task2 first!</h2>
 				<h3>Username: Vichea and Password: 123456</h3>
